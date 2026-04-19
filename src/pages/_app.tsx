10d716a4a63278065ced4570c876b2e779766bf2
Baturay Duman <95505048+baturaydmn@users.noreply.github.com>
2026-04-19 22:58:19 +0300

    google search console added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app'
-import { LanguageProvider, useLanguage } from '../contexts/LanguageContext'
+import Script from 'next/script'
+import { LanguageProvider } from '../contexts/LanguageContext'
 import { ThemeProvider } from '../contexts/ThemeContext'
 import '../styles/globals.css'
 
@@ -7,6 +8,19 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
       <LanguageProvider>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-62VQTYKTQ3"
+        />
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-62VQTYKTQ3');
+          `}
+        </Script>
         <Component {...pageProps} />
       </LanguageProvider>
     </ThemeProvider>
